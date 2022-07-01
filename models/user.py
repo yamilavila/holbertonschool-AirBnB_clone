fb450afce7a6518767aaaf02adf625ce16cf17cb
Joanfa7 <jafabre95@gmail.com>
2022-07-01 14:55:41 +0000

    adding new file

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" File contains the user class """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ creates a user basic info """
+    def __init__(self):
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
