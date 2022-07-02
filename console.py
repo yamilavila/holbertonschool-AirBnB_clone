92eb0602e37e9e93cd0fd33fc50ac5c426a63956
Joanfa7 <jafabre95@gmail.com>
2022-07-02 01:54:17 +0000

    adding new file

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ console contains the enery point of the command interpreter """
 import cmd
+from types import new_class
+from models import classes
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +22,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ when user pres <ENTER> nothing is executed  """
         pass
+    
+    def do_create(self, command):
+        if command is None:
+            print("** class name missing **")
+        elif command == "BaseModel":
+            n_class = classes[command]()
+            print(n_class.id)
+            storage.new(n_class)
+            storage.save()
+        else:
+            print("** class doesn't exist **")
+
 
 
 if __name__ == '__main__':
