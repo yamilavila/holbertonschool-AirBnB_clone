b163c8ccfcd916ad85b2c50cb8212c7503812598
Joanfa7 <jafabre95@gmail.com>
2022-07-04 01:51:31 +0000

    quick fix

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ creates a user basic info """
-    def __init__(self):
         self.email = ""
         self.password = ""
         self.first_name = ""
