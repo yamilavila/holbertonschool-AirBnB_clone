02d8111f36d8f5f991a0eee7d1259e2d3291e9cd
Joanfa7 <jafabre95@gmail.com>
2022-07-05 19:26:17 +0000

    add test_console file

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/nin/python3
+""" Unittest for Console """
+import unittest
+
+
+class TestUserClass(unittest.TestCase):
+    """ class Console """
+
+if __name__ == "__main__":
+    unittest.main()
