8f9f6ec443697eb970f452a8b9c5e7ace08b90e1
Joanfa7 <jafabre95@gmail.com>
2022-07-05 23:10:53 +0000

    update basemodel test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,58 +2,44 @@
 """
 Test model for BaseModel class
 """
-from models.base_model import BaseModel
+from test_base_model import BaseModel
+from datetime import datetime
+from models.engine.file_storage import FileStorage
 import unittest
-from models import storage
-import pycodestyle
+from models.base_model import BaseModel
+from unittest.case import _AssertRaisesContext
 
 
 class TestBaseModelClass(unittest.TestCase):
-    """
-    unittest for BaseModel
-    """
+    """ Class test Base Model """
+
     def setUp(self):
-        self.new_instance_1 = BaseModel()
-        self.new_instance_2 = BaseModel()
+       """ create base models """
+       self.x_base_model = BaseModel()
+       self.y_base_model = BaseModel()
 
-    def testInit(self):
-        self.assertIsInstance(self.new_instance_1, BaseModel)
-        self.assertIsInstance(self.new_instance_2, BaseModel)
+    def test_str_v_str(self):
+        """ compare two stirng """
+        self.assertIsInstance(self.x_base_model.__str__(), str)
 
-    def testCreate(self):
-        self.before_save = self.new_instance_1.update_at
-        self.new_instance_1.save()
-        self.after_save = self.new_instance_1.update_at
+    def test_str_info(self):
+        """ check the info of the string"""
+        self.assertIn("[BaseModel]", self.y_base_model.__str__())
 
-    def testUpdate(self):
-        self.before_update = self.new_instance_2.created_at
-        self.new_instance_2.save()
-        self.after_update = self.new_instance_2.created_at
-        self.assertEqual(str(self.before_update), str(self.after_update))
+    def test_Init(self):
+        """ check if init is working """
+        self.assertIsInstance(self.x_base_model, BaseModel)
+        self.assertIsInstance(self.y_base_model, BaseModel)
 
-    def testDict(self):
-        self.dict = dict(self.new_instance_1.__dict__)
-        self.dict['__class__'] = "BaseModel"
-        self.dict['created_at'] =\
-            self.dict['created_at'].isoformat()
-        self.dict['updated_at'] =\
-            self.dict['updated_at'] =\
-            self.assertDictEqual(self.dict, self.new_instance_1.to_dict())
+    def test_id(self):
+        """ check if all the insatnce hast it own id """
+        self.assertNotEqual(self.x_base_model.id,
+                            self.y_base_model.id)
 
+    def test_kwarg(self):
+        objdict = self.x_base_model.to_dict()
+        self.kwarginstance = BaseModel(**objdict)
+        self.assertIsInstance(self.kwarginstance, BaseModel)
 
-class CodeStyleTest(unittest.TestCase):
-    """
-    Pycodestyle test
-    """
-
-    def testPycodestyle(self):
-        """
-        Test cases for pycodestyle
-        """
-    code_style = pycodestyle.StyleGuide(quiet=True)
-    result = code_style.check_files(["models/base_model.py"])
-    self.assertEqual(result.total_errors, 0, "BaseModel pycodestyle error")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
