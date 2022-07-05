3504b20a39e9f93d018fc5f2efd6fa0bc8d4bab7
yamilavila <git@github.com>
2022-07-05 19:13:18 +0000

    Unittest incomplete

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -6,4 +6,23 @@ from models.amenity import Amenity
 import unittest
 
 
-classTestInit
+class TestUser(unittest.TestCase):
+    """Testing Amenity and the functions
+    """
+    def testInit(self):
+        """
+        """
+        self.new_amenity = Amenity()
+        self.assertIsIntance(self.new_amenity, Amenity)
+
+    def testID(self):
+        """
+        """
+        self.new_id_1 = Amenity()
+        self.new_id_2 = Amenity()
+        self.assertNotEqual(self.new_id_1.id, self.new_id_2.id)
+
+    def testSave(self):
+        """
+        """
+        self.save
