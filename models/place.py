c50dfc3ed81164a727ea7a894a090c35493d05ab
Joanfa7 <jafabre95@gmail.com>
2022-07-06 03:22:50 +0000

    fix place.py typo

--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ class Place(BaseModel):
     city_id = ""
     user_id = ""
     name = ""
-    descritpion = ""
+    description = ""
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
