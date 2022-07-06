cbbd77fffc426cddf97b1243cd426e48a7d3fbe8
Joanfa7 <jafabre95@gmail.com>
2022-07-06 03:31:03 +0000

    test console

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbdb) '
-    
+
     """This first 3 are the basic commands for the interpreter"""
 
     def emptyline(self):
@@ -17,32 +17,13 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_EOF(self, command):
-        """ End of file.. Ends program """
+        """ End of file.. Ends program \n"""
         return True
 
     def do_quit(self, command):
-        """ Quit command to exit the program """
-        self.close()
-        quit()
+        """ Quit command to exit the program\n """
+        return True
 
-    def help_quit(self):
-        print('\n'.join(['Quit command to exit the program\n']))
-
-    def close(self):
-        if self.string:
-            self.string.close()
-            self.string = None
-
-   
-    def do_count(self, command):
-        """count the instance of a class"""
-        ctr = 0
-        if command not in classes:
-            print("**class name missing**")
-        for key in storage.all.keys():
-            if comand in key:
-                cnt += 1
-        print(ctr)
 
 
 if __name__ == "__main__":
