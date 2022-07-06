c284eb96f310d33d39f46e51cb4d04998d24e5bb
Joanfa7 <jafabre95@gmail.com>
2022-07-06 01:47:53 +0000

    fix typo

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel():
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.update_at = datetime.now()
+            self.updated_at = datetime.now()
 
         else:
             mat = "%Y-%m-%dT%H:%M:%S.%f"
