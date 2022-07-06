36bcf7e2cb42e17b7123ba8dd44fef1371ce3e02
Joanfa7 <jafabre95@gmail.com>
2022-07-06 03:00:38 +0000

    check if user work

--- models/user.py
+++ models/user.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ creates a user basic info """
+    """class User """
 
+    last_name = ""
     email = ""
     password = ""
     first_name = ""
-    last_name = ""
